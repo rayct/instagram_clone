978751740cec498f88d87ce9c91c834814d9ba35
rayct <rayct@icloud.com>
2022-07-22 01:39:36 +0100

    Added WebApp Version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,21 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBA1gu6wfLxU8hueL7oJT6V4OruJ-Igvlo",
+          appId: "1:134100803884:web:b99c4441ef585ed62bcc89",
+          messagingSenderId: "134100803884",
+          projectId: "instagram-clone-1282e",
+          storageBucket: 'instagram-clone-1282e.appspot.com'),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
