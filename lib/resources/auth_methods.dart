9ce8075e89b105c36b5453c59c8ac04d06260e17
rayct <rayct@icloud.com>
2022-08-04 01:33:05 +0100

    LOGIN AND SIGN-UP SCREENS COMPLETED

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,12 +1,13 @@
-// import 'dart:typed_data';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:instagram_clone/models/user.dart' as model;
-// import 'package:instagram_clone/resources/storage_methods.dart';
+// import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:typed_data';
+import 'package:instagram_clone/models/user.dart' as model;
+import 'package:instagram_clone/resources/storage_methods.dart';
 
-// class AuthMethods {
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
+class AuthMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
 //   // get user details
 //   Future<model.User> getUserDetails() async {
@@ -19,55 +20,57 @@
 //   }
 
 //   // Signing Up User
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint8List file,
+  }) async {
+    String res = "Some error Occurred";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        // registering user in auth with email and password
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
 
-//   Future<String> signUpUser({
-//     required String email,
-//     required String password,
-//     required String username,
-//     required String bio,
-//     required Uint8List file,
-//   }) async {
-//     String res = "Some error Occurred";
-//     try {
-//       if (email.isNotEmpty ||
-//           password.isNotEmpty ||
-//           username.isNotEmpty ||
-//           bio.isNotEmpty ||
-//           file != null) {
-//         // registering user in auth with email and password
-//         UserCredential cred = await _auth.createUserWithEmailAndPassword(
-//           email: email,
-//           password: password,
-//         );
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
 
-//         String photoUrl = await StorageMethods()
-//             .uploadImageToStorage('profilePics', file, false);
+        model.User _user = model.User(
+          username: username,
+          uid: cred.user!.uid,
+          photoUrl: photoUrl,
+          email: email,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
 
-//         model.User _user = model.User(
-//           username: username,
-//           uid: cred.user!.uid,
-//           photoUrl: photoUrl,
-//           email: email,
-//           bio: bio,
-//           followers: [],
-//           following: [],
-//         );
+        // adding user in our database
+        await _firestore
+            .collection("users")
+            .doc(cred.user!.uid)
+            .set(_user.toJson());
 
-//         // adding user in our database
-//         await _firestore
-//             .collection("users")
-//             .doc(cred.user!.uid)
-//             .set(_user.toJson());
+        res = "success";
+      } else {
+        res = "Please enter all the fields";
+      }
+    } catch (err) {
+      return err.toString();
+    }
+    return res;
+  }
 
-//         res = "success";
-//       } else {
-//         res = "Please enter all the fields";
-//       }
-//     } catch (err) {
-//       return err.toString();
-//     }
-//     return res;
-//   }
+  loginUser({required String email, required String password}) {}
+}
 
 //   // logging in user
 //   Future<String> loginUser({
@@ -77,6 +80,7 @@
 //     String res = "Some error Occurred";
 //     try {
 //       if (email.isNotEmpty || password.isNotEmpty) {
+
 //         // logging in user with email and password
 //         await _auth.signInWithEmailAndPassword(
 //           email: email,
