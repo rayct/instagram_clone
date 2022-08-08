7c2246ddade9f73d9c2bc0d5e183cc3de9b3abf1
rayct <rayct@icloud.com>
2022-08-08 02:39:53 +0100

    Completed Sign up and Login

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
