1d58edc4ed3490d4a08151f4e532de23a68f8b11
rayct <rayct@icloud.com>
2022-08-10 01:57:51 +0100

    Persisting Auth State

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 // import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
-// import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/responsive/responsive_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
-// import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+// import 'package:instagram_clone/providers/user_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,7 +49,34 @@ class MyApp extends StatelessWidget {
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
       // ),
-      home: LoginScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            // Checking if the snapshot has any data or not
+            if (snapshot.hasData) {
+              // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+              return const ResponsiveLayout(
+                mobileScreenLayout: MobileScreenLayout(),
+                webScreenLayout: WebScreenLayout(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+          }
+          // means connection to future hasnt been made yet
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+                child: CircularProgressIndicator(
+              color: primaryColor,
+            ));
+          }
+
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
@@ -97,19 +123,19 @@ class MyApp extends StatelessWidget {
 //                   mobileScreenLayout: MobileScreenLayout(),
 //                   webScreenLayout: WebScreenLayout(),
 //                 );
-//               } else if (snapshot.hasError) {
-//                 return Center(
-//                   child: Text('${snapshot.error}'),
-//                 );
-//               }
-//             }
+            //   } else if (snapshot.hasError) {
+            //     return Center(
+            //       child: Text('${snapshot.error}'),
+            //     );
+            //   }
+            // }
 
 //             // means connection to future hasnt been made yet
-//             if (snapshot.connectionState == ConnectionState.waiting) {
-//               return const Center(
-//                 child: CircularProgressIndicator(),
-//               );
-//             }
+            // if (snapshot.connectionState == ConnectionState.waiting) {
+            //   return const Center(
+            //     child: CircularProgressIndicator(),
+            //   );
+            // }
 
 //             return const LoginScreen();
 //           },
